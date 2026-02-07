34bc3a55ab988c84b886fae7dea7bb0b032d453f
dwaipayanbasu <dwaipayan.basu@gmail.com>
2026-02-08 00:31:56 +0530

    Stopped rotation of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -13,6 +14,12 @@ import 'services/voice_recording_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Lock orientation to portrait only
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   // Initialize Stripe
   Stripe.publishableKey = ApiConfig.stripePublishableKey;
